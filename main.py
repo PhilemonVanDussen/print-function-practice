9bca95d5e8c78092e7b9baf32b510b8f11103e03
PhilemonVanDussen <vandussenph709@northwested.org>
2024-09-09 14:23:30 -0400

    Updated my script

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print('hello')
-print('good afternoon')
+# print('hello')
+# print('good afternoon')
+# personal_message = "Hi, im PJ VanDussen"
+
+
+# print(personal_message)
+# print(famous_quote)
+
+famous_quote = 'A penny saved is a penny earned.'
+famous_person = 'PJ VanDussen'
+
+# message = f'{famous_person} once said: {famous_quote}'
+# print(message)
+
+print(f'{famous_person} once said: {famous_quote}')
